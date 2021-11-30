d28617bebc23deb936753414e9a6c56c515c7f7d
giacoletti <giacoletti92@gmail.com>
2021-11-30 22:08:51 +0100

    Adds request tests in show route if trying to reach an article with invalid id - expected to return status 404 and error message

--- spec/requests/articles/show_spec.rb
+++ spec/requests/articles/show_spec.rb
@@ -1,13 +1,13 @@
 describe 'GET /api/articles/:id' do
+  let!(:article) {
+    create(:article,
+           title: 'This is the latest news',
+           content: 'And this is some content')
+  }
+  
+  subject { response }
+ 
   describe 'successfully' do
-    subject { response }
-
-    let!(:article) {
-      create(:article,
-             title: 'This is the latest news',
-             content: 'And this is some content')
-    }
-
     before do
       get "/api/articles/#{article.id}"
     end
@@ -22,6 +22,21 @@ describe 'GET /api/articles/:id' do
 
     it 'is expected to respond with the requested article body' do
       expect(response_json['content']).to eq 'And this is some content'
+    end
+  end
+
+  describe 'unsuccessfully with invalid id' do
+    before do
+      get '/api/articles/abc'
+    end
+
+    it 'is expected to respond with status 404' do
+      binding.pry
+      expect(subject.status).to eq 404
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['message']).to eq 'Unfortunately we cannot find the article you are looking for.'
     end
   end
 end
