0d38a744d32b55cf5810e8e7dbe4548cf536b53f
giacoletti <giacoletti92@gmail.com>
2021-11-30 19:05:08 +0100

    Adds validation to 'title' in Article model minor request test refactoring - is expected to return 3 articles

--- spec/requests/articles/index_spec.rb
+++ spec/requests/articles/index_spec.rb
@@ -1,7 +1,7 @@
 describe 'GET /api/articles' do
   describe 'successfully' do
     subject { response }
-    let!(:article) { create(:article, title: 'My Own Title') }
+    let!(:article) { 3.times { create(:article, title: 'My Own Title') } }
 
     before do
       get '/api/articles'
@@ -11,11 +11,11 @@ describe 'GET /api/articles' do
       expect(subject.status).to eq 200
     end
 
-    it 'is expected to return all articles' do
+    it 'is expected to return 3 articles' do
       expect(response_json.count).to eq 3
     end
 
-    it 'is expected to respond with an article' do
+    it 'is expected to respond with an article with title "My Own Title"' do
       expect(response_json.first['title']).to eq 'My Own Title'
     end
   end
