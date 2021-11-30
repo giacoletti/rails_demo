edc6cf83bc24bb3d88165e87218b0aa1f12a1f21
giacoletti <giacoletti92@gmail.com>
2021-11-30 20:53:47 +0100

    Adds show route to config/routes.rb adds show method to articles_controller.rb

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,4 +3,9 @@ class Api::ArticlesController < ApplicationController
     articles = Article.all
     render json: articles
   end
+
+  def show
+    article = Article.find(params['id'])
+    render json: article
+  end
 end
